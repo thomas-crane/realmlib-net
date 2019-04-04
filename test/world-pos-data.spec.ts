9855ccaa7b2fc32ceb46f030ae32c836237e32a4
thomas-crane <thomasgcrane@gmail.com>
2019-04-04 19:37:24 +1100

    test(world-pos-data): update tests

--- test/world-pos-data.spec.ts
+++ test/world-pos-data.spec.ts
@@ -1,27 +1,27 @@
 import { expect } from 'chai';
 import 'mocha';
 
-import { Point } from '../src';
+import { WorldPosData } from '../src';
 
-describe('Point', () => {
+describe('WorldPosData', () => {
   it('should be initialised at the origin if no args are provided.', () => {
-    const p = new Point();
+    const p = new WorldPosData();
     expect(p.x).to.equal(0, 'Incorrect initial x.');
     expect(p.y).to.equal(0, 'Incorrect initial y.');
   });
   it('should be initialised at the provided args.', () => {
-    const p = new Point(3, 4);
+    const p = new WorldPosData(3, 4);
     expect(p.x).to.equal(3, 'Incorrect initial x.');
     expect(p.y).to.equal(4, 'Incorrect initial y.');
   });
   describe('#squareDistanceTo()', () => {
     it('should return the correct squared distance for valid inputs.', () => {
-      const a = new Point();
-      const b = new Point(3, 4);
+      const a = new WorldPosData();
+      const b = new WorldPosData(3, 4);
       expect(a.squareDistanceTo(b)).to.equal(25, 'Incorrect result for squareDistanceTo.');
     });
     it('should throw a TypeError for invalid inputs.', () => {
-      const a = new Point();
+      const a = new WorldPosData();
       expect(() => a.squareDistanceTo(421 as any)).to.throw(TypeError);
       expect(() => a.squareDistanceTo('Test' as any)).to.throw(TypeError);
       expect(() => a.squareDistanceTo(null)).to.throw(TypeError);
@@ -29,19 +29,19 @@ describe('Point', () => {
   });
   describe('#distanceTo()', () => {
     it('should return the correct distance for valid inputs.', () => {
-      const a = new Point();
-      const b = new Point(3, 4);
+      const a = new WorldPosData();
+      const b = new WorldPosData(3, 4);
       expect(a.distanceTo(b)).to.equal(5, 'Incorrect result for distanceTo.');
     });
     it('should throw a TypeError for invalid inputs.', () => {
-      const a = new Point();
+      const a = new WorldPosData();
       expect(() => a.distanceTo(8231 as any)).to.throw(TypeError);
       expect(() => a.distanceTo('Hello, World!' as any)).to.throw(TypeError);
       expect(() => a.distanceTo(null)).to.throw(TypeError);
     });
   });
   describe('#clone()', () => {
-    const a = new Point(10, 23);
+    const a = new WorldPosData(10, 23);
     const b = a.clone();
     it('should return a new point with the same coordinates.', () => {
       expect(b.x).to.equal(a.x, 'Incorrect x value.');
