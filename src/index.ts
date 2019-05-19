96654444cb1315febc25b8875059a6aed581ec1a
thomas-crane <thomasgcrane@gmail.com>
2019-05-19 18:53:49 +1000

    chore(packets): replace static class
    This commit replaces the static Packets class with a single exported
    function `createPacket`. The behaviour of this method is exactly the
    same as the old one, except that the new one will throw if the requested
    packet type is unknown.

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
 // auto generated with tsai
 // https://github.com/thomas-crane/tsai
+export * from './create-packet';
 export * from './packet-map';
 export * from './packet-type';
 export * from './packet';
 export * from './packetio';
-export * from './packets';
 export * from './reader';
 export * from './writer';
 export * from './crypto/index';
